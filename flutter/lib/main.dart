156061b63d1f6cceecadb51d0ec1fbe9307b4a8a
Danny Vaca <danny270793@icloud.com>
2026-02-05 14:15:37 -0500

    Update main.dart with go_router implementation and authentication flow

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,47 +1,126 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:phone_finder/data/auth/auth_api.dart';
-import 'package:phone_finder/data/auth/auth_repository_impl.dart';
-import 'package:phone_finder/data/auth/user_storage.dart';
-import 'package:phone_finder/domain/auth/login_usecase.dart';
-import 'package:phone_finder/domain/auth/logout_usecase.dart';
+import 'package:go_router/go_router.dart';
+import 'package:phone_finder/data/login/login_api.dart';
+import 'package:phone_finder/data/login/login_repository_impl.dart';
+import 'package:phone_finder/data/login/login_storage.dart';
+import 'package:phone_finder/domain/router/router_usecase.dart';
+import 'package:phone_finder/domain/login/login_usecase.dart';
+import 'package:phone_finder/domain/login/logout_usecase.dart';
+import 'package:phone_finder/state/router/router_cubit.dart';
+import 'package:phone_finder/state/router/router_state.dart';
 import 'package:phone_finder/state/login/login_cubit.dart';
 import 'package:phone_finder/state/login/logout_cubit.dart';
-import 'package:phone_finder/ui/login/login_page.dart';
+import 'package:phone_finder/ui/pages/error_page.dart';
+import 'package:phone_finder/ui/pages/home_page.dart';
+import 'package:phone_finder/ui/pages/login_page.dart';
+import 'package:phone_finder/ui/pages/splash_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
   final prefs = await SharedPreferences.getInstance();
-  final storage = UserStorage(prefs);
+  final storage = LoginStorage(prefs);
   final api = AuthApi();
-  final repo = AuthRepositoryImpl(api, storage);
+  final repo = LoginRepositoryImpl(api, storage);
   final loginUseCase = LoginUseCase(repo);
   final logoutUseCase = LogoutUseCase(repo);
-  
+  final routerUseCase = RouterUseCase(repo);
+
+  final routerCubit = RouterCubit(routerUseCase);
   runApp(
     MultiBlocProvider(
       providers: [
         BlocProvider(create: (_) => LoginCubit(loginUseCase)),
         BlocProvider(create: (_) => LogoutCubit(logoutUseCase)),
+        BlocProvider(create: (_) => routerCubit),
       ],
-      child: const MyApp(),
+      child: MyApp(routerCubit: routerCubit),
     ),
   );
 }
 
+final protectedPages = const [
+  HomePage.routeName,
+];
+final publicPages = const [
+  LoginPage.routeName,
+];
+
+class GoRouterRefreshStream extends ChangeNotifier {
+  late final StreamSubscription _subscription;
+
+  GoRouterRefreshStream(Stream stream) {
+    _subscription = stream.listen((_) => notifyListeners());
+  }
+
+  @override
+  void dispose() {
+    _subscription.cancel();
+    super.dispose();
+  }
+}
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final RouterCubit routerCubit;
+
+  const MyApp({super.key, required this.routerCubit});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return MaterialApp.router(
+      routerConfig: GoRouter(
+        initialLocation: SplashPage.routeName,
+        refreshListenable: GoRouterRefreshStream(routerCubit.stream),
+        redirect: (context, state) {
+          final routerState = context.read<RouterCubit>().state;
+          if(routerState is RouterInitSuccess) {
+            final location = state.matchedLocation;
+            final isLoggedIn = routerState.isLogged;
+
+            if(location == SplashPage.routeName) {
+              if(isLoggedIn) {
+                return HomePage.routeName;
+              } else {
+                return LoginPage.routeName;
+              }
+            }
+
+            final isPublicPage = publicPages.contains(location);
+            final isProtectedPage = protectedPages.contains(location);
+
+            if (isProtectedPage && !isLoggedIn) {
+              return LoginPage.routeName;
+            }
+
+            if (isPublicPage && isLoggedIn) {
+              return HomePage.routeName;
+            }
+
+            return null;
+          } else if(routerState is RouterInitError) {
+            return ErrorPage.routeName;
+          } else {
+            return SplashPage.routeName;
+          }
+        },
+        routes: [
+          GoRoute(path: SplashPage.routeName, builder: (context, state) => const SplashPage()),
+          GoRoute(path: LoginPage.routeName, builder: (context, state) => LoginPage()),
+          GoRoute(path: HomePage.routeName, builder: (context, state) => const HomePage()),
+          GoRoute(path: ErrorPage.routeName, builder: (context, state) => const ErrorPage()),
+        ],
       ),
-      home: LoginPage(),
+      theme: ThemeData.light(
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData.dark(
+        useMaterial3: true,
+      ),
     );
   }
 }
