9e14ad521489b81a490d5e77c7e850a3b7d690a8
Danny Vaca <danny270793@icloud.com>
2026-02-05 15:44:33 -0500

    Integrate locale management into app with reactive language switching

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:phone_finder/config/app_routes.dart';
 import 'package:phone_finder/data/login/login_api.dart';
 import 'package:phone_finder/data/login/login_repository_impl.dart';
 import 'package:phone_finder/data/login/login_storage.dart';
+import 'package:phone_finder/data/settings/locale_storage.dart';
 import 'package:phone_finder/domain/router/router_usecase.dart';
 import 'package:phone_finder/domain/login/login_usecase.dart';
 import 'package:phone_finder/domain/login/logout_usecase.dart';
@@ -11,6 +12,8 @@ import 'package:phone_finder/l10n/app_localizations.dart';
 import 'package:phone_finder/state/router/router_cubit.dart';
 import 'package:phone_finder/state/login/login_cubit.dart';
 import 'package:phone_finder/state/login/logout_cubit.dart';
+import 'package:phone_finder/state/settings/locale_cubit.dart';
+import 'package:phone_finder/state/settings/locale_state.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
@@ -26,6 +29,7 @@ void main() async {
 
   final prefs = await SharedPreferences.getInstance();
   final storage = LoginStorage(prefs);
+  final localeStorage = LocaleStorage(prefs);
   final api = AuthApi();
   final repo = LoginRepositoryImpl(api, storage);
   final loginUseCase = LoginUseCase(repo);
@@ -33,11 +37,16 @@ void main() async {
   final routerUseCase = RouterUseCase(repo);
 
   final routerCubit = RouterCubit(routerUseCase);
+  final localeCubit = LocaleCubit(localeStorage);
+  
+  // Load saved locale
+  await localeCubit.loadLocale();
 
   runApp(
     MultiBlocProvider(
       providers: [
         BlocProvider.value(value: routerCubit),
+        BlocProvider.value(value: localeCubit),
         BlocProvider(create: (_) => LoginCubit(loginUseCase)),
         BlocProvider(create: (_) => LogoutCubit(logoutUseCase)),
       ],
@@ -53,18 +62,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRoutes.getRouter(routerCubit),
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: AppLocalizations.supportedLocales,
-      onGenerateTitle: (context) => AppLocalizations.of(context)!.appTitle,
-      theme: ThemeData.light(useMaterial3: true),
-      darkTheme: ThemeData.dark(useMaterial3: true),
+    return BlocBuilder<LocaleCubit, LocaleState>(
+      builder: (context, localeState) {
+        // Get the current locale from cubit
+        final locale = context.read<LocaleCubit>().currentLocale;
+
+        return MaterialApp.router(
+          routerConfig: AppRoutes.getRouter(routerCubit),
+          locale: locale,
+          localizationsDelegates: const [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: AppLocalizations.supportedLocales,
+          onGenerateTitle: (context) => AppLocalizations.of(context)!.appTitle,
+          theme: ThemeData.light(useMaterial3: true),
+          darkTheme: ThemeData.dark(useMaterial3: true),
+        );
+      },
     );
   }
 }
