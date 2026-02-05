4c731cdb7a9745d8f97ce9e88dfdcd935040b816
Danny Vaca <danny270793@icloud.com>
2026-02-05 11:36:36 -0500

    Initialize user storage and provide logout cubit in main

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -2,18 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:phone_finder/data/auth/auth_api.dart';
 import 'package:phone_finder/data/auth/auth_repository_impl.dart';
+import 'package:phone_finder/data/auth/user_storage.dart';
 import 'package:phone_finder/domain/auth/login_usecase.dart';
+import 'package:phone_finder/domain/auth/logout_usecase.dart';
 import 'package:phone_finder/state/login/login_cubit.dart';
+import 'package:phone_finder/state/login/logout_cubit.dart';
 import 'package:phone_finder/ui/login/login_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  final prefs = await SharedPreferences.getInstance();
+  final storage = UserStorage(prefs);
   final api = AuthApi();
-  final repo = AuthRepositoryImpl(api);
+  final repo = AuthRepositoryImpl(api, storage);
   final loginUseCase = LoginUseCase(repo);
+  final logoutUseCase = LogoutUseCase(repo);
   
   runApp(
-    BlocProvider(
-      create: (_) => LoginCubit(loginUseCase),
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => LoginCubit(loginUseCase)),
+        BlocProvider(create: (_) => LogoutCubit(logoutUseCase)),
+      ],
       child: const MyApp(),
     ),
   );
@@ -27,7 +39,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: LoginPage(),
     );
