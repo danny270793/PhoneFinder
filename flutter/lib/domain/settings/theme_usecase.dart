e064a2b401a67bfaa3f250ed518f1d0b7ad48e06
Danny Vaca <danny270793@icloud.com>
2026-02-05 16:17:56 -0500

    Add ThemeUseCase to encapsulate theme business logic

---
+++ flutter/lib/domain/settings/theme_usecase.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:phone_finder/domain/settings/theme_repository.dart';
+
+class ThemeUseCase {
+  final ThemeRepository _repository;
+
+  ThemeUseCase(this._repository);
+
+  Future<void> saveThemeMode(ThemeMode mode) async {
+    await _repository.saveThemeMode(mode);
+  }
+
+  ThemeMode? getThemeMode() {
+    return _repository.getThemeMode();
+  }
+
+  Future<void> clearThemeMode() async {
+    await _repository.clearThemeMode();
+  }
+
+  ThemeMode getThemeModeOrDefault() {
+    final savedTheme = _repository.getThemeMode();
+    if (savedTheme != null) {
+      return savedTheme;
+    }
+    return ThemeMode.system;
+  }
+}
