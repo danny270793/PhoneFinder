df97d17371db29c63f705c0ea4297e637c16fbc3
Danny Vaca <danny270793@icloud.com>
2026-02-05 14:58:15 -0500

    Add generated localization files

---
+++ flutter/lib/l10n/app_localizations_en.dart
@@ -0,0 +1,40 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get appTitle => 'Phone Finder';
+
+  @override
+  String get login => 'Login';
+
+  @override
+  String get logout => 'Logout';
+
+  @override
+  String get home => 'Home';
+
+  @override
+  String get error => 'Error';
+
+  @override
+  String get email => 'Email';
+
+  @override
+  String get password => 'Password';
+
+  @override
+  String get emailHint => 'Enter your email';
+
+  @override
+  String get passwordHint => 'Enter your password';
+
+  @override
+  String get loading => 'Loading...';
+}
---
+++ flutter/lib/l10n/app_localizations_es.dart
@@ -0,0 +1,40 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Spanish Castilian (`es`).
+class AppLocalizationsEs extends AppLocalizations {
+  AppLocalizationsEs([String locale = 'es']) : super(locale);
+
+  @override
+  String get appTitle => 'Buscador de Teléfonos';
+
+  @override
+  String get login => 'Iniciar Sesión';
+
+  @override
+  String get logout => 'Cerrar Sesión';
+
+  @override
+  String get home => 'Inicio';
+
+  @override
+  String get error => 'Error';
+
+  @override
+  String get email => 'Correo Electrónico';
+
+  @override
+  String get password => 'Contraseña';
+
+  @override
+  String get emailHint => 'Ingrese su correo electrónico';
+
+  @override
+  String get passwordHint => 'Ingrese su contraseña';
+
+  @override
+  String get loading => 'Cargando...';
+}
