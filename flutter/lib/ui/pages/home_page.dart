dcf4fe0ee585989ad785572cbfd2e22e2189c7b6
Danny Vaca <danny270793@icloud.com>
2026-02-05 14:15:31 -0500

    Add home_page.dart in pages folder with routing

---
+++ flutter/lib/ui/pages/home_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:phone_finder/state/login/logout_cubit.dart';
+import 'package:phone_finder/state/login/logout_state.dart';
+import 'package:phone_finder/ui/pages/login_page.dart';
+
+class HomePage extends StatelessWidget {
+  static const routeName = '/home';
+
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home')),
+      body: BlocConsumer<LogoutCubit, LogoutState>(
+        listener: (context, state) {
+          if (state is LogoutError) {
+            ScaffoldMessenger.of(context)
+                .showSnackBar(SnackBar(content: Text(state.message)));
+          } else if (state is LogoutSuccess) {
+            Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => LoginPage()));
+          }
+        },
+        builder: (context, state) {
+          return Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+                children: [
+                  ElevatedButton(
+                  onPressed: state is LogoutRequested
+                      ? null
+                      : () {
+                          context.read<LogoutCubit>().logout();
+                        },
+                  child: state is LogoutRequested
+                      ? const CircularProgressIndicator()
+                      : const Text('Logout'),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
