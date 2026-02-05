1e2f49ecc0cd1dc5f7957e5826bce3de3ffc3377
Danny Vaca <danny270793@icloud.com>
2026-02-05 11:23:29 -0500

    Add login cubit for state management

---
+++ flutter/lib/state/login/login_cubit.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:phone_finder/domain/auth/auth_repository.dart';
+import 'package:phone_finder/domain/auth/login_usecase.dart';
+import 'package:phone_finder/state/login/login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  final LoginUseCase loginUseCase;
+
+  LoginCubit(this.loginUseCase) : super(LoginIdle());
+
+  Future<void> login(String email, String password) async {
+    emit(LoginRequested());
+
+    try {
+      final User user = await loginUseCase.execute(email, password);
+      emit(LoginSuccess());
+    } catch (e) {
+      emit(LoginError(e.toString()));
+    }
+  }
+}
