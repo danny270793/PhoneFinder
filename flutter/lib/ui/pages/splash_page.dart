efeaafae18232be944d3e590610edc7d87d95cf4
Danny Vaca <danny270793@icloud.com>
2026-02-05 14:48:32 -0500

    Refactor SplashPage to improve readability by consolidating the Scaffold widget structure into a single line

--- flutter/lib/ui/pages/splash_page.dart
+++ flutter/lib/ui/pages/splash_page.dart
@@ -7,10 +7,6 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: CircularProgressIndicator(),
-      ),
-    );
+    return const Scaffold(body: Center(child: CircularProgressIndicator()));
   }
 }
