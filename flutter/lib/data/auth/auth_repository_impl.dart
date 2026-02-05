e5deb6ba8b34bd3d578cbef29fe2e3602316ee97
Danny Vaca <danny270793@icloud.com>
2026-02-05 11:36:17 -0500

    Update auth repository to store user on login and retrieve on logout

--- flutter/lib/data/auth/auth_repository_impl.dart
+++ flutter/lib/data/auth/auth_repository_impl.dart
@@ -1,11 +1,12 @@
 import 'package:phone_finder/data/auth/auth_api.dart' as auth_api;
+import 'package:phone_finder/data/auth/user_storage.dart';
 import 'package:phone_finder/domain/auth/auth_repository.dart';
-
 
 class AuthRepositoryImpl implements AuthRepository {
   final auth_api.AuthApi api;
+  final UserStorage storage;
 
-  AuthRepositoryImpl(this.api);
+  AuthRepositoryImpl(this.api, this.storage);
 
   @override
   Future<User> login({
@@ -13,6 +14,13 @@ class AuthRepositoryImpl implements AuthRepository {
     required String password,
   }) async {
     final auth_api.User user = await api.login(email, password);
+    
+    // Store user data locally
+    await storage.saveUser(
+      accessToken: user.accessToken,
+      refreshToken: user.refreshToken,
+    );
+    
     return User(
       accessToken: user.accessToken,
       refreshToken: user.refreshToken,
@@ -20,11 +28,33 @@ class AuthRepositoryImpl implements AuthRepository {
   }
 
   @override
-  Future<void> logout(User user) async {
-    final auth_api.User apiUser = auth_api.User(
-      accessToken: user.accessToken,
-      refreshToken: user.refreshToken,
+  Future<void> logout() async {
+    // Get the stored user
+    final userData = await storage.getUser();
+    
+    if (userData != null) {
+      final auth_api.User apiUser = auth_api.User(
+        accessToken: userData['accessToken']!,
+        refreshToken: userData['refreshToken']!,
+      );
+      await api.logout(apiUser);
+    }
+    
+    // Clear stored user data
+    await storage.clearUser();
+  }
+
+  @override
+  Future<User?> getCurrentUser() async {
+    final userData = await storage.getUser();
+    
+    if (userData == null) {
+      return null;
+    }
+    
+    return User(
+      accessToken: userData['accessToken']!,
+      refreshToken: userData['refreshToken']!,
     );
-    await api.logout(apiUser);
   }
 }
