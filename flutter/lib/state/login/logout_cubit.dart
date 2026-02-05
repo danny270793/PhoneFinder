9a34d964c739d5717013c676fcbb1405cfab107b
Danny Vaca <danny270793@icloud.com>
2026-02-05 11:32:09 -0500

    Add logout cubit for state management

---
+++ flutter/lib/state/login/logout_cubit.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:phone_finder/domain/auth/auth_repository.dart';
+import 'package:phone_finder/domain/auth/logout_usecase.dart';
+import 'package:phone_finder/state/login/logout_state.dart';
+
+class LogoutCubit extends Cubit<LogoutState> {
+  final LogoutUseCase logoutUseCase;
+
+  LogoutCubit(this.logoutUseCase) : super(LogoutIdle());
+
+  Future<void> logout(User user) async {
+    emit(LogoutRequested());
+
+    try {
+      await logoutUseCase.execute(user);
+      emit(LogoutSuccess());
+    } catch (e) {
+      emit(LogoutError(e.toString()));
+    }
+  }
+}
