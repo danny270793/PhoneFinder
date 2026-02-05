7bb7cd63725f5b3e7ebc45038e6b86de6b4995d8
Danny Vaca <danny270793@icloud.com>
2026-02-05 14:47:44 -0500

    Refactor User class constructor in login_api.dart for improved readability

--- flutter/lib/data/login/login_api.dart
+++ flutter/lib/data/login/login_api.dart
@@ -1,10 +1,7 @@
 class User {
   final String accessToken;
   final String refreshToken;
-  User({
-    required this.accessToken,
-    required this.refreshToken,
-  });
+  User({required this.accessToken, required this.refreshToken});
 }
 
 class AuthApi {
@@ -13,11 +10,9 @@ class AuthApi {
     if (email != 'email@example.com' || password != 'password') {
       throw Exception('Unauthorized');
     }
-    return User(
-      accessToken: 'accessToken',
-      refreshToken: 'refreshToken',
-    );
+    return User(accessToken: 'accessToken', refreshToken: 'refreshToken');
   }
+
   Future<void> logout(User user) async {
     await Future.delayed(const Duration(seconds: 1));
   }
