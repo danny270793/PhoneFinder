571b2d54b88bf26469831361ff977a66c16771a1
Danny Vaca <danny270793@icloud.com>
2026-02-05 11:47:04 -0500

    Change login password validation from '1234' to 'password'

--- flutter/lib/data/auth/auth_api.dart
+++ flutter/lib/data/auth/auth_api.dart
@@ -10,7 +10,7 @@ class User {
 class AuthApi {
   Future<User> login(String email, String password) async {
     await Future.delayed(const Duration(seconds: 1));
-    if (password != '1234') {
+    if (password != 'password') {
       throw Exception('Unauthorized');
     }
     return User(
