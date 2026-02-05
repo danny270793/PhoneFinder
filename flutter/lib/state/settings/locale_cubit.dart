2830e1bb01aaa225cb291bf7cab0c75f90471d59
Danny Vaca <danny270793@icloud.com>
2026-02-05 16:06:48 -0500

    Update LocaleCubit to depend on LocaleRepository interface

--- flutter/lib/state/settings/locale_cubit.dart
+++ flutter/lib/state/settings/locale_cubit.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:phone_finder/data/settings/locale_storage.dart';
+import 'package:phone_finder/domain/settings/locale_repository.dart';
 import 'package:phone_finder/state/settings/locale_state.dart';
 
 class LocaleCubit extends Cubit<LocaleState> {
-  final LocaleStorage _storage;
+  final LocaleRepository _repository;
 
-  LocaleCubit(this._storage) : super(const LocaleInitial()) {}
+  LocaleCubit(this._repository) : super(const LocaleInitial()) {}
 
   /// Load the saved locale from storage
   Future<void> loadLocale() async {
-    final savedLocale = _storage.getLocale();
+    final savedLocale = _repository.getLocale();
     if (savedLocale != null) {
       emit(LocaleLoaded(Locale(savedLocale)));
     } else {
@@ -21,7 +21,7 @@ class LocaleCubit extends Cubit<LocaleState> {
 
   /// Change the locale and save it
   Future<void> changeLocale(String languageCode) async {
-    await _storage.saveLocale(languageCode);
+    await _repository.saveLocale(languageCode);
     emit(LocaleChanged(Locale(languageCode)));
   }
 
