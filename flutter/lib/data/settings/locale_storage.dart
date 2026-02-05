717888e053283c4fa8a2360888b9d3469c4bb908
Danny Vaca <danny270793@icloud.com>
2026-02-05 16:06:46 -0500

    Refactor LocaleStorage to implement LocaleRepository interface

--- flutter/lib/data/settings/locale_storage.dart
+++ flutter/lib/data/settings/locale_storage.dart
@@ -1,22 +1,23 @@
+import 'package:phone_finder/domain/settings/locale_repository.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class LocaleStorage {
+class LocaleStorageImpl implements LocaleRepository {
   static const _localeKey = 'selected_locale';
   final SharedPreferences _prefs;
 
-  LocaleStorage(this._prefs);
+  LocaleStorageImpl(this._prefs);
 
-  /// Save the selected locale language code (e.g., 'en', 'es')
+  @override
   Future<void> saveLocale(String languageCode) async {
     await _prefs.setString(_localeKey, languageCode);
   }
 
-  /// Get the saved locale language code, returns null if not set
+  @override
   String? getLocale() {
     return _prefs.getString(_localeKey);
   }
 
-  /// Clear the saved locale
+  @override
   Future<void> clearLocale() async {
     await _prefs.remove(_localeKey);
   }
