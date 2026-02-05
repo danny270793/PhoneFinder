10ce4c57d5fcbc6e5775580ff1e48a846f42c49b
Danny Vaca <danny270793@icloud.com>
2026-02-05 15:00:10 -0500

    Refactor ErrorPage widget for improved readability by consolidating widget structure into a single line

--- flutter/lib/ui/pages/error_page.dart
+++ flutter/lib/ui/pages/error_page.dart
@@ -9,11 +9,7 @@ class ErrorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final l10n = AppLocalizations.of(context)!;
-    
-    return Scaffold(
-      body: Center(
-        child: Text(l10n.error),
-      ),
-    );
+
+    return Scaffold(body: Center(child: Text(l10n.error)));
   }
 }
