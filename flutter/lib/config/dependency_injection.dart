f9ea3eb5fe698bc8710664c681b34413364164b5
Danny Vaca <danny270793@icloud.com>
2026-02-05 16:29:54 -0500

    Add dependency injection configuration using get_it

---
+++ flutter/lib/config/dependency_injection.dart
@@ -0,0 +1,100 @@
+import 'package:get_it/get_it.dart';
+import 'package:phone_finder/data/login/login_api.dart';
+import 'package:phone_finder/data/login/login_repository.dart';
+import 'package:phone_finder/data/login/login_repository_impl.dart';
+import 'package:phone_finder/data/settings/locale_storage.dart';
+import 'package:phone_finder/data/settings/theme_storage.dart';
+import 'package:phone_finder/data/storage/shared_preferences_impl.dart';
+import 'package:phone_finder/domain/login/login_usecase.dart';
+import 'package:phone_finder/domain/login/logout_usecase.dart';
+import 'package:phone_finder/domain/router/router_usecase.dart';
+import 'package:phone_finder/domain/settings/locale_repository.dart';
+import 'package:phone_finder/domain/settings/locale_usecase.dart';
+import 'package:phone_finder/domain/settings/theme_repository.dart';
+import 'package:phone_finder/domain/settings/theme_usecase.dart';
+import 'package:phone_finder/domain/storage/preferences_repository.dart';
+import 'package:phone_finder/state/login/login_cubit.dart';
+import 'package:phone_finder/state/login/logout_cubit.dart';
+import 'package:phone_finder/state/router/router_cubit.dart';
+import 'package:phone_finder/state/settings/locale_cubit.dart';
+import 'package:phone_finder/state/settings/theme_cubit.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final getIt = GetIt.instance;
+
+Future<void> configureDependencies() async {
+  // External dependencies
+  final prefs = await SharedPreferences.getInstance();
+  getIt.registerSingleton<SharedPreferences>(prefs);
+
+  // Data layer - Storage
+  getIt.registerLazySingleton<PreferencesRepository>(
+    () => SharedPreferencesImpl(getIt<SharedPreferences>()),
+  );
+
+  // Data layer - API
+  getIt.registerLazySingleton<AuthApi>(() => AuthApi());
+
+  // Data layer - Repositories
+  getIt.registerLazySingleton<LoginRepository>(
+    () => LoginRepositoryImpl(
+      getIt<AuthApi>(),
+      getIt<PreferencesRepository>(),
+    ),
+  );
+
+  getIt.registerLazySingleton<LocaleRepository>(
+    () => LocaleStorageImpl(getIt<PreferencesRepository>()),
+  );
+
+  getIt.registerLazySingleton<ThemeRepository>(
+    () => ThemeStorageImpl(getIt<PreferencesRepository>()),
+  );
+
+  // Domain layer - Use cases
+  getIt.registerLazySingleton<LoginUseCase>(
+    () => LoginUseCase(getIt<LoginRepository>()),
+  );
+
+  getIt.registerLazySingleton<LogoutUseCase>(
+    () => LogoutUseCase(getIt<LoginRepository>()),
+  );
+
+  getIt.registerLazySingleton<RouterUseCase>(
+    () => RouterUseCase(getIt<LoginRepository>()),
+  );
+
+  getIt.registerLazySingleton<LocaleUseCase>(
+    () => LocaleUseCase(getIt<LocaleRepository>()),
+  );
+
+  getIt.registerLazySingleton<ThemeUseCase>(
+    () => ThemeUseCase(getIt<ThemeRepository>()),
+  );
+
+  // State layer - Cubits
+  getIt.registerFactory<LoginCubit>(
+    () => LoginCubit(getIt<LoginUseCase>()),
+  );
+
+  getIt.registerFactory<LogoutCubit>(
+    () => LogoutCubit(getIt<LogoutUseCase>()),
+  );
+
+  getIt.registerLazySingleton<RouterCubit>(
+    () => RouterCubit(getIt<RouterUseCase>()),
+  );
+
+  getIt.registerLazySingleton<LocaleCubit>(
+    () => LocaleCubit(getIt<LocaleUseCase>()),
+  );
+
+  getIt.registerLazySingleton<ThemeCubit>(
+    () => ThemeCubit(getIt<ThemeUseCase>()),
+  );
+
+  // Initialize cubits that need it
+  await getIt<RouterCubit>().init();
+  await getIt<LocaleCubit>().loadLocale();
+  await getIt<ThemeCubit>().loadThemeMode();
+}
