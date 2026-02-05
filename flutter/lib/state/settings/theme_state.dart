4d9e345d335452083359154af8a3078e6aadc11e
Danny Vaca <danny270793@icloud.com>
2026-02-05 16:13:13 -0500

    Clean up ThemeState code formatting

--- flutter/lib/state/settings/theme_state.dart
+++ flutter/lib/state/settings/theme_state.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
 
-/// Base class for theme state
 abstract class ThemeState {
   const ThemeState();
 }
 
-/// Initial state
 class ThemeInitial extends ThemeState {
   const ThemeInitial();
 }
 
-/// Theme loaded from storage
 class ThemeLoaded extends ThemeState {
   final ThemeMode themeMode;
 
   const ThemeLoaded(this.themeMode);
 }
 
-/// Theme changed by user
 class ThemeChanged extends ThemeState {
   final ThemeMode themeMode;
 
