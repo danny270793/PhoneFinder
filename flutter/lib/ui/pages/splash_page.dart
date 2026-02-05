1da89efa06b84fc27de68646224a26f9b222da4e
Danny Vaca <danny270793@icloud.com>
2026-02-05 14:46:34 -0500

    Update splash_page with inline route name

--- flutter/lib/ui/pages/splash_page.dart
+++ flutter/lib/ui/pages/splash_page.dart
@@ -1,26 +1,13 @@
-
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:phone_finder/state/router/router_cubit.dart';
 
-class SplashPage extends StatefulWidget {
-  static const routeName = '/';
+class SplashPage extends StatelessWidget {
+  static const routeName = '/splash';
 
   const SplashPage({super.key});
 
   @override
-  State<SplashPage> createState() => SplashPageState();
-}
-
-class SplashPageState extends State<SplashPage> {
-  @override
-  void initState() {
-    super.initState();
-    context.read<RouterCubit>().init();
-  }
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
         child: CircularProgressIndicator(),
       ),
