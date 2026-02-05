e43861413698793c1fac3aef6dd226ecb75eb1e1
Danny Vaca <danny270793@icloud.com>
2026-02-05 14:46:40 -0500

    Update error_page with inline route name

--- flutter/lib/ui/pages/error_page.dart
+++ flutter/lib/ui/pages/error_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class ErrorPage extends StatelessWidget {
@@ -8,7 +7,7 @@ class ErrorPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
         child: Text('Error'),
       ),
