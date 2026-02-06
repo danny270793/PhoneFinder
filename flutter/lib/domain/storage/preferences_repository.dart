0475ddea5fab4c8a9252f6d2c9935e010458195f
Danny Vaca <danny270793@icloud.com>
2026-02-06 14:09:45 -0500

    Make all PreferencesRepository methods return Future

--- flutter/lib/domain/storage/preferences_repository.dart
+++ flutter/lib/domain/storage/preferences_repository.dart
@@ -1,6 +1,6 @@
 abstract class PreferencesRepository {
   Future<void> setString({required String key, required String value});
-  String? getString({required String key});
+  Future<String?> getString({required String key});
   Future<void> remove({required String key});
-  bool containsKey({required String key});
+  Future<bool> containsKey({required String key});
 }
