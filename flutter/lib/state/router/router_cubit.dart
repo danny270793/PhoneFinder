31e733a51b2ec269f9dbfec9490808df6c0440c0
Danny Vaca <danny270793@icloud.com>
2026-02-06 13:38:51 -0500

    Update RouterCubit to extend BaseCubit and use safeExecute

--- flutter/lib/state/router/router_cubit.dart
+++ flutter/lib/state/router/router_cubit.dart
@@ -1,8 +1,8 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:phone_finder/domain/router/router_usecase.dart';
+import 'package:phone_finder/state/base_cubit.dart';
 import 'package:phone_finder/state/router/router_state.dart';
 
-class RouterCubit extends Cubit<RouterState> {
+class RouterCubit extends BaseCubit<RouterState> {
   final RouterUseCase routerUseCase;
 
   RouterCubit(this.routerUseCase) : super(RouterIdle()) {}
@@ -10,13 +10,16 @@ class RouterCubit extends Cubit<RouterState> {
   Future<void> init() async {
     emit(RouterCheckAuthRequested());
 
-    try {
-      await Future.delayed(const Duration(seconds: 1));
-      final isLogged = await routerUseCase.execute();
-      emit(RouterCheckAuthSuccess(isLogged));
-    } catch (e) {
-      emit(RouterCheckAuthError(e.toString()));
-    }
+    await safeExecute(
+      () async {
+        await Future.delayed(const Duration(seconds: 1));
+        final isLogged = await routerUseCase.execute();
+        emit(RouterCheckAuthSuccess(isLogged));
+      },
+      onError: (error, stackTrace) {
+        emit(RouterCheckAuthError(error.toString()));
+      },
+    );
   }
 
   void onLoginSuccess() {
