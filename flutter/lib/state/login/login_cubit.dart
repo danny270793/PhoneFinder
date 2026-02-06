9238cb2896d9ce6edcc3d34cda5b4f171fd5699f
Danny Vaca <danny270793@icloud.com>
2026-02-06 13:48:41 -0500

    Revert "Update LoginCubit to extend BaseCubit and use safeExecute"
    This reverts commit 7da9fc147af2f4ed8b075c89ca4fa009948bf054.

--- flutter/lib/state/login/login_cubit.dart
+++ flutter/lib/state/login/login_cubit.dart
@@ -1,8 +1,8 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:phone_finder/domain/login/login_usecase.dart';
-import 'package:phone_finder/state/base_cubit.dart';
 import 'package:phone_finder/state/login/login_state.dart';
 
-class LoginCubit extends BaseCubit<LoginState> {
+class LoginCubit extends Cubit<LoginState> {
   final LoginUseCase loginUseCase;
 
   LoginCubit(this.loginUseCase) : super(LoginIdle());
@@ -10,14 +10,11 @@ class LoginCubit extends BaseCubit<LoginState> {
   Future<void> login(String email, String password) async {
     emit(LoginRequested());
 
-    await safeExecute(
-      () async {
-        await loginUseCase.execute(email, password);
-        emit(LoginSuccess());
-      },
-      onError: (error, stackTrace) {
-        emit(LoginError(error.toString()));
-      },
-    );
+    try {
+      await loginUseCase.execute(email, password);
+      emit(LoginSuccess());
+    } catch (e) {
+      emit(LoginError(e.toString()));
+    }
   }
 }
