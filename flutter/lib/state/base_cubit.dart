20577ef7025c2fd22170d2563ff9e9f9e0c98b9d
Danny Vaca <danny270793@icloud.com>
2026-02-06 13:42:15 -0500

    Add abstract createErrorState method to BaseCubit for automatic error state emission

--- flutter/lib/state/base_cubit.dart
+++ flutter/lib/state/base_cubit.dart
@@ -5,7 +5,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 abstract class BaseCubit<State> extends Cubit<State> {
   BaseCubit(super.initialState);
 
+  /// Create an error state for this cubit
+  /// Each cubit must implement this to define its error state
+  State createErrorState(String message);
+
   /// Safe execute wrapper that catches errors and reports them
+  /// Automatically emits error state on failure
   Future<void> safeExecute(
     Future<void> Function() action, {
     void Function(Object error, StackTrace stackTrace)? onError,
@@ -14,11 +19,17 @@ abstract class BaseCubit<State> extends Cubit<State> {
       await action();
     } catch (error, stackTrace) {
       addError(error, stackTrace);
-      onError?.call(error, stackTrace);
+      if (onError != null) {
+        onError(error, stackTrace);
+      } else {
+        // Automatically emit error state if no custom handler provided
+        emit(createErrorState(error.toString()));
+      }
     }
   }
 
   /// Safe execute with return value
+  /// Automatically emits error state on failure
   Future<T?> safeExecuteWithResult<T>(
     Future<T> Function() action, {
     void Function(Object error, StackTrace stackTrace)? onError,
@@ -27,7 +38,12 @@ abstract class BaseCubit<State> extends Cubit<State> {
       return await action();
     } catch (error, stackTrace) {
       addError(error, stackTrace);
-      onError?.call(error, stackTrace);
+      if (onError != null) {
+        onError(error, stackTrace);
+      } else {
+        // Automatically emit error state if no custom handler provided
+        emit(createErrorState(error.toString()));
+      }
       return null;
     }
   }
