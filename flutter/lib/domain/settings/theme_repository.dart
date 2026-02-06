897aa20c691bc6a81ba8254aad0ab0bba01c4e3f
Danny Vaca <danny270793@icloud.com>
2026-02-06 14:10:05 -0500

    Make ThemeRepository.getThemeMode return Future

--- flutter/lib/domain/settings/theme_repository.dart
+++ flutter/lib/domain/settings/theme_repository.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 abstract class ThemeRepository {
   Future<void> saveThemeMode({required ThemeMode mode});
 
-  ThemeMode? getThemeMode();
+  Future<ThemeMode?> getThemeMode();
 
   Future<void> clearThemeMode();
 }
